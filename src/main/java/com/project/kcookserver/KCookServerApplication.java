86e61ade9c208440b71643ee69726ea6425ab703
vividswan <vividswan@naver.com>
2021-08-15 12:37:30 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/com/project/kcookserver/KCookServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.kcookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KCookServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KCookServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/kcookserver/KCookServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kcookserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KCookServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
