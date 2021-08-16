5acf7c20f3bcd12bd04a3f9f522dd82fe9376eef
vividswan <vividswan@naver.com>
2021-08-17 00:14:52 +0900

    Add: Spring Security 및 인증관련 UserDetails, UserDetailsService 구현 클래스 추가

---
+++ src/main/java/com/project/kcookserver/configure/security/SecurityConfiguration.java
@@ -0,0 +1,85 @@
+package com.project.kcookserver.configure.security;
+
+import com.project.kcookserver.configure.response.exception.CustomAuthenticationEntryPoint;
+import com.project.kcookserver.configure.security.jwt.JwtAuthenticationFilter;
+import com.project.kcookserver.configure.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+    
+
+    @Value("${mapping.url}")
+    private String mappingUrl;
+
+
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins(mappingUrl)
+                    .allowedMethods("GET", "POST", "OPTIONS", "PUT");
+        }
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs/**", "/swagger-resources/**",
+                "/swagger-ui/**", "/webjars/**", "/swagger/**","/swagger.json");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.POST,"/app/sign-in", "/app/sign-up").permitAll()
+                    .antMatchers(HttpMethod.GET, "/errors/**").permitAll()
+                    .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                    .anyRequest().hasRole("USER")
+                .and()
+                    .exceptionHandling()
+                    .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .and()
+                    .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
---
+++ src/main/java/com/project/kcookserver/configure/security/authentication/CustomUserDetailService.java
@@ -0,0 +1,30 @@
+package com.project.kcookserver.configure.security.authentication;
+
+import com.project.kcookserver.account.AccountRepository;
+import com.project.kcookserver.account.entity.Account;
+import com.project.kcookserver.configure.response.exception.CustomException;
+import com.project.kcookserver.configure.response.exception.CustomExceptionStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static com.project.kcookserver.configure.entity.Status.*;
+
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String signInId) {
+        Optional<Account> optionalAccount = accountRepository.findBySignInIdAndStatus(signInId, VALID);
+        if (!optionalAccount.isPresent()) throw new CustomException(CustomExceptionStatus.ACCOUNT_NOT_FOUND);
+        return new CustomUserDetails(optionalAccount.get());
+    }
+
+}
