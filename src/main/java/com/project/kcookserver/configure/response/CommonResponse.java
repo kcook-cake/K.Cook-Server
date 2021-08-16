d0c22c4ba1ac743cd6a966d973e0c7f51307131b
vividswan <vividswan@naver.com>
2021-08-16 16:47:48 +0900

    Feat: Response 기능 관련 객체 추가

---
+++ src/main/java/com/project/kcookserver/configure/response/CommonResponse.java
@@ -0,0 +1,19 @@
+package com.project.kcookserver.configure.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CommonResponse {
+
+    protected Boolean isSuccess;
+
+    protected int code;
+
+    protected String message;
+
+}
