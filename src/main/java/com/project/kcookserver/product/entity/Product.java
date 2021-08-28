dff43bdf484daafcf88534e63f44cac8d417978c
vividswan <vividswan@naver.com>
2021-08-29 00:40:13 +0900

    Add: Product 칼럼에 판매량(salesRate) 추가

--- src/main/java/com/project/kcookserver/product/entity/Product.java
+++ src/main/java/com/project/kcookserver/product/entity/Product.java
@@ -44,6 +44,8 @@ public class Product extends BaseTimeEntity {
 
     private Boolean isCake;
 
+    private Integer salesRate;
+
     @OneToMany(mappedBy = "product")
     private List<Review> reviews = new ArrayList<>();
 
