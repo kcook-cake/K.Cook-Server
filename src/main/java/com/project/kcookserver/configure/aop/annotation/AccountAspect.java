9272729cf7962dcf373929e036d004cd7edec7e8
vividswan <vividswan@naver.com>
2021-10-06 20:22:30 +0900

    Feat: Account Log 관련 Aspect 고도화 및 로그를 DB에 저장하는 기능 추가

--- src/main/java/com/project/kcookserver/configure/aop/annotation/AccountAspect.java
+++ src/main/java/com/project/kcookserver/configure/aop/annotation/AccountAspect.java
@@ -1,6 +1,10 @@
 package com.project.kcookserver.configure.aop.annotation;
 
+import com.project.kcookserver.account.entity.Account;
+import com.project.kcookserver.configure.Repository.AccountLogRepository;
+import com.project.kcookserver.configure.entity.AccountLog;
 import com.project.kcookserver.configure.security.authentication.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -9,30 +13,41 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Component
+@RequiredArgsConstructor
 @Aspect
 public class AccountAspect {
 
     Logger logger = LoggerFactory.getLogger(AccountAspect.class);
+    private final AccountLogRepository accountLogRepository;
 
     @Around("@annotation(com.project.kcookserver.configure.aop.annotation.AccountLog)")
     public Object AccountLogAspect(ProceedingJoinPoint joinPoint) throws Throwable {
         Object proceed = joinPoint.proceed();
 
+        Account account = null;
         String userName = "anonymousUser";
         String userRole = "null";
+        Long accountd = null;
 
         if (!SecurityContextHolder.getContext()
                 .getAuthentication()
                 .getPrincipal().equals("anonymousUser")) {
             CustomUserDetails user = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            account = user.getAccount();
             userName = user.getUsername();
             userRole = user.getAccount().getRole().toString();
+            accountd = user.getAccount().getAccountId();
         }
 
-        logger.info(userName);
-        logger.info(userRole);
-        logger.info(joinPoint.getSignature().getName());
+        LocalDateTime localDateTime = LocalDateTime.now();
+        logger.info("--Authenticated Account Access-- [Date : "+localDateTime.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"))+"] [AccountId : "+accountd+"] [UserName : "+userName+"] [UserRole : "+userRole+"] [Method : "+joinPoint.getSignature().getName()+"]");
+        AccountLog accountLog = new AccountLog(account, localDateTime, joinPoint.getSignature().getName());
+
+        accountLogRepository.save(accountLog);
 
         return proceed;
     }
